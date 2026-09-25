1484492432439eb09b510381b27661611ebb45b2
Your Name <harshipsph344@gmail.com>
2026-09-25 18:50:30 +0530

    Update backend API URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://releif-mesh1.onrender.com/api';
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
